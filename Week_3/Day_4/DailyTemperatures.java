051ed3185ca7a6c143b18b342401254b3c81bfff
Palak Varshney <varshneypalak11@gmail.com>
2026-06-18 22:26:38 +0530

    Week_3 Day4 Solutions

---
+++ Week_3/Day_4/DailyTemperatures.java
@@ -0,0 +1,20 @@
+lass Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        int n = temp.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.isEmpty() && temp[i] > temp[st.peek()]) {
+                int idx = st.pop();
+                res[idx] = i - idx;
+            }
+
+            st.push(i);
+        }
+
+        return res;
+    }
+}
+    
---
+++ Week_3/Day_4/EvaluateReversePolishNotation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<tokens.length; i++){
+            String token = tokens[i];
+            if(token.equals("+")){
+                st.push(st.pop() + st.pop());
+            }
+            else if(token.equals("-")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a-b);
+            }
+            else if(token.equals("*")){
+                st.push(st.pop() * st.pop());
+            }
+            else if(token.equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a/b);
+            }
+            else{
+                st.push(Integer.parseInt(token));
+            }
+        }
+        return st.pop();
+    }
+}
